7e5b2998f09a73db2d3e5bbeefcc3290d8852be5
Youssef Ali Mokhtar <youssef96mokhtar@gmail.com>
2024-03-20 09:39:18 +0200

    create and post post data to server

--- frontend/src/pages/CreatePost.jsx
+++ frontend/src/pages/CreatePost.jsx
@@ -1,8 +1,67 @@
+import { useState } from 'react';
 import CreatePostClasses from './create-post.module.css';
+import { useNavigate } from 'react-router-dom';
 
 const CreatePost = () => {
+    const [postInput, setPostInput] = useState({title:'', description:''});
+    const navigate = useNavigate();
+
+    const handleInput = (event)=> {
+        setPostInput(prev=>({
+            ...prev, 
+            [event.target.name]: event.target.value
+        }));
+    }
+
+    const handleSubmitInput = (event)=> {
+        event.preventDefault();
+        fetch('http://localhost:4000/api/posts', {
+            method:'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(postInput)
+        })
+        .then(response=> {
+            console.log(response);
+            setPostInput({title:'', description:''});
+            navigate('/');
+        })
+        .catch(err=>{
+            console.log(err);
+        })
+        console.log("Heey!!");
+    }
+
+    console.log(postInput);
     return ( <div className={CreatePostClasses['create-post']}>
-        Create Post!!
+        <form 
+            className={CreatePostClasses['input-container']}
+            onSubmit={handleSubmitInput}
+            >
+            <h1 className={CreatePostClasses['create-post-title']}>Create Post</h1>
+            <input 
+                className={CreatePostClasses['input-title']}
+                type='text'
+                name='title'
+                placeholder='Title'
+                value={postInput.title}
+                onChange={handleInput}
+                required
+                />
+            <textarea
+                className={CreatePostClasses['input-description']}
+                type='text'
+                name='description'
+                placeholder='Speak your mind...'
+                value={postInput.description}
+                onChange={handleInput}
+                required
+            />
+            <div className={CreatePostClasses['input-button-holder']}>
+                <button className={CreatePostClasses['input-button']}>Post</button>
+            </div>
+        </form>
     </div> );
 }
  
