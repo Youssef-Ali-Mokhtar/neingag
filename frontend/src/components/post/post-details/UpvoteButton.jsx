16ebc19b256cebf04c38df5bee701a6f395a13b0
Youssef Ali Mokhtar <youssef96mokhtar@gmail.com>
2024-04-14 00:06:14 +0200

    add voting

--- frontend/src/components/post/post-details/UpvoteButton.jsx
+++ frontend/src/components/post/post-details/UpvoteButton.jsx
@@ -1,38 +1,10 @@
 import { BiUpvote } from "react-icons/bi";
 import { BiSolidUpvote } from "react-icons/bi";
-import { useEffect } from "react";
-import { useAuthContext } from "../../../hooks/useAuthContext";
-import { useParams } from "react-router-dom";
 import UpvoteClasses from './../post.module.css';
 
-const UpvoteButton = ({ postId, upvote, setUpvote, handleUpvote, upvoteNum }) => {
-    
-    const { user } = useAuthContext();
-    const id = useParams().id || postId;
+const UpvoteButton = ({ upvote, handleUpvote, upvoteNum }) => {
 
-
-
-    useEffect(()=>{
-        const setUpvoteInitialState = ()=>{
-            fetch(`http://localhost:4000/api/users/upvotes/${id}`, {
-                headers: {
-                    'Authorization':`Bearer ${user?.token}`
-                }
-            })
-            .then(response=> {
-                return response.json();
-            })
-            .then(isUpvoted=> {
-                setUpvote(isUpvoted);
-            })
-            .catch(err=> {
-                console.log(err);
-            })
-        }
-        setUpvoteInitialState();
-
-    }, [id, user?.token]);
-
+    const isPressedClass = upvote? UpvoteClasses['pressed']:'';
 
     const upvoteCheckedIcon = (
         <BiUpvote
@@ -46,7 +18,7 @@ const UpvoteButton = ({ postId, upvote, setUpvote, handleUpvote, upvoteNum }) =>
             size={20}
         />)
 
-    return ( <div className={UpvoteClasses['vote-button']}>
+    return ( <div className={`${UpvoteClasses['vote-button']} ${isPressedClass}`}>
         {upvote? upvoteUncheckedIcon:upvoteCheckedIcon }
         <p>{upvoteNum}</p>
     </div> );
