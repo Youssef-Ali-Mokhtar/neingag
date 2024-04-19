6e98281acdfdd7de139eed1fda6fbcd0e634f3ee
Youssef Ali Mokhtar <youssef96mokhtar@gmail.com>
2024-04-19 23:10:56 +0200

    edit cors origin path

--- backend/socket.js
+++ backend/socket.js
@@ -4,7 +4,7 @@ module.exports = {
     init: httpServer=> {
             io = require('socket.io')(httpServer, {
             cors: {
-                origin: process.env.CLIENT_URL
+                origin:"*"
             }
         });
         return io;
